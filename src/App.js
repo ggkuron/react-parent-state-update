0b2e4813536d821857469eb23f60819cae4c9644
ggkuron <ggkuron@gmail.com>
2020-08-10 05:33:33 +0900

    parent state update from child

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Parent />
     </div>
   );
+}
+
+export function Parent() {
+  const [state, updater] = React.useState(new Map([[0, 0], [1, 0]]));
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column"
+      }}
+    >
+      {
+        Array.from(state.entries()).map(
+          ([id, v]) =>
+            <Child
+              key={id}
+              id={id}
+              value={v}
+              updater={updater}
+            />
+        )
+      }
+    </div>
+  )
+}
+
+function Child({
+  id,
+  value,
+  updater,
+}) {
+  const handleClick = React.useCallback(() =>
+    updater(prev => new Map([...prev.entries(), [id, prev.get(id) + 1]])),
+    [id, updater]
+  )
+  return (
+    <div>
+      <span>{value}</span>
+      <button
+        onClick={handleClick}
+      >update</button>
+    </div>
+  )
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import Enzyme, { shallow, mount } from 'enzyme'
+import EnzymeAdapter from 'enzyme-adapter-react-16'
+
+import App, { Parent } from './App';
+
+Enzyme.configure({ adapter: new EnzymeAdapter() })
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const wrapper = mount(<Parent />)
+  expect(wrapper.render().find("button")).toHaveLength(2);
 });
